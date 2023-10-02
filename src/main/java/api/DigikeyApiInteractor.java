939261a9d98a6039ac6e8cdd5a976b45fe47b1e9
Corknut <grassyolive@gmail.com>
2023-10-01 23:36:52 -0400

    fixed api call (still needs to have oauth set up to work) made gitignore and update pom.xml

--- src/main/java/api/DigikeyApiInteractor.java
+++ src/main/java/api/DigikeyApiInteractor.java
@@ -3,22 +3,26 @@ package api;
 import okhttp3.*;
 import java.io.IOException;
 
+import org.json.JSONObject;
+
 // Calling the Code in Java to Run the API and Get the Response for the Product Detail.
 
 public class DigikeyApiInteractor {
+    public static String oauthToken = "Bearer q7zAohcObSXrzGq3VARRboqDCrPS"; // needs to be set dynamically
     public static void main(String[] args) throws IOException {
         OkHttpClient client = new OkHttpClient().newBuilder()
                 .build();
         MediaType mediaType = MediaType.parse("text/plain");
-        RequestBody body = RequestBody.create(mediaType, "");
         Request request = new Request.Builder()
                 .url("https://api.digikey.com/Search/v3/Products/380-BC-HH006F-ND")
-                .method("GET", body)
+                .method("GET", null)
                 .addHeader("X-DIGIKEY-Client-Id", "Rpr2ezZg6K0Mag1sR5ROpS0A69JuZAjH")
-                .addHeader("Authorization", "Bearer q7zAohcObSXrzGq3VARRboqDCrPS")
+                .addHeader("Authorization", oauthToken)
                 .addHeader("X-DIGIKEY-Locale-Currency", "CAD")
-                .addHeader("Cookie", "TS012a951d=01460246b6c15d8aa2405e4fc2e5f7bea0e24e6e2aba5dd279e3ab336c076d6355f5f0b76ccf626bcaa2793f905dc550d10e341a1a")
                 .build();
         Response response = client.newCall(request).execute();
+
+        JSONObject responseObject = new JSONObject(response.body().string());
+        System.out.println(responseObject.toString());
     }
 }
