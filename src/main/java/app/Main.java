e82de3bc5f9acb6064d2d65940c7173b6c87af15
Corknut <grassyolive@gmail.com>
2023-10-30 15:51:47 -0400

    small changes and a test in main

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,20 +1,42 @@
 package app;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 import data_access.database.DatabaseAccessInterface;
 import data_access.database.PostGreSQLAccessObject;
+import data_access.pokemon.PokemonCardDataAccessInterface;
 import data_access.pokemon.TestCardDataAccessObject;
 import data_access.database.TestDatabaseAccessObject;
+import data_access.image.ImageCacheAccessInterface;
 import entity.Card;
 
 import usecase.PokemonTCGApiUseCase;
 
 public class Main {
     public static void main(String[] args) {
-        PokemonTCGApiUseCase pokemonTCGApiUseCase = new PokemonTCGApiUseCase();
+        // Data access init
+
+        PokemonCardDataAccessInterface pokemonCardDAO = new TestCardDataAccessObject();
+        ImageCacheAccessInterface imageDAO = (ImageCacheAccessInterface) pokemonCardDAO;
         DatabaseAccessInterface db = new TestDatabaseAccessObject();//new PostGreSQLAccessObject();
 
+
+        // Usercase
+
+        PokemonTCGApiUseCase pokemonTCGApiUseCase = new PokemonTCGApiUseCase(pokemonCardDAO);
+
+        // Other
+
         GUIManager guiManager = new GUIManager();
-        Card c = pokemonTCGApiUseCase.apiInteractor.searchCards("mega lopunny").get(0);
+        //Card c = pokemonTCGApiUseCase.apiInteractor.searchCards("mega lopunny").get(0);
+        Card c = pokemonTCGApiUseCase.apiAccessObject.searchCards("Horse").get(0);
         System.out.println(c.name);
+
+        JLabel jLabel = new JLabel(new ImageIcon(imageDAO.getImage(c.imageURL)));
+        jLabel.setSize(245, 342);
+        ((JPanel)(guiManager.views.getComponent(0))).add(jLabel);
+        guiManager.views.repaint();
     }
 }
--- src/main/java/usecase/PokemonTCGApiUseCase.java
+++ src/main/java/usecase/PokemonTCGApiUseCase.java
@@ -1,7 +1,13 @@
 package usecase;
 
+import data_access.pokemon.PokemonCardDataAccessInterface;
 import data_access.pokemon.PokemonGuruDataAccessObject;
 
 public class PokemonTCGApiUseCase {
-    public PokemonGuruDataAccessObject apiInteractor = new PokemonGuruDataAccessObject();
+    // TODO: Make this private, its public for now to test
+    public PokemonCardDataAccessInterface apiAccessObject;
+
+    public PokemonTCGApiUseCase(PokemonCardDataAccessInterface dataAccessObject) {
+        apiAccessObject = dataAccessObject;
+    }
 }
