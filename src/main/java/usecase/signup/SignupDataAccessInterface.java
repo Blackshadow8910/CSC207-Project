825d2e0a903bc5df5829767c52305066e9955186
liolive4 <grassyolive@gmail.com>
2023-11-13 17:42:24 -0500

    added registerUser method to SignupDataAccessInterface.java

--- src/main/java/usecase/signup/SignupDataAccessInterface.java
+++ src/main/java/usecase/signup/SignupDataAccessInterface.java
@@ -1,9 +1,10 @@
 package usecase.signup;
 
+import data_access.database.UserAlreadyExistsException;
 import entity.User;
 
 public interface SignupDataAccessInterface {
     public User getUser(String username);
 
-    public void registerUser(String username, String password);
+    public void registerUser(String username, String password) throws UserAlreadyExistsException;
 }
