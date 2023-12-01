750697b642be854f842e41a44b3cd274d91435e5
LAPTOP-S89BI6Q2\justi <justi.yu@mail.utoronto.ca>
2023-12-01 05:03:20 -0500

    added test for Login Feature

---
+++ src/test/java/LoginInteractorTest.java
@@ -0,0 +1,55 @@
+
+
+import entity.User;
+import org.junit.Before;
+import org.junit.Test;
+import usecase.login.*;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
+
+public class LoginInteractorTest {
+    private LoginDataAccessInterface databaseAccessObject;
+    private LoginOutputBoundary presenter;
+    private LoginInteractor loginInteractor;
+
+    @Before
+    public void setUp() {
+        // Create mock objects for dependencies
+        databaseAccessObject = mock(LoginDataAccessInterface.class);
+        presenter = mock(LoginOutputBoundary.class);
+
+        // Initialize the LoginInteractor with mock variables
+        loginInteractor = new LoginInteractor(databaseAccessObject, presenter);
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        // Arrange
+        LoginInputData inputData = new LoginInputData("validUsername", "validPassword");
+        User mockedUser = new User("validUsername", "hashedValidPassword"); // Assuming a hashed password
+
+        // Mock the behavior of the databaseAccessObject
+        when(databaseAccessObject.getUser("validUsername", "validPassword")).thenReturn(mockedUser);
+
+        loginInteractor.login(inputData);
+
+        // Assert
+        verify(presenter).presentSuccess(any(LoginOutputData.class));
+    }
+
+    @Test
+    public void testLoginFailure() {
+        // Arrange
+        LoginInputData inputData = new LoginInputData("invalidUsername", "invalidPassword");
+
+        // Mock the behavior of the databaseAccessObject
+        when(databaseAccessObject.getUser("invalidUsername", "invalidPassword")).thenReturn(null);
+
+        // Act
+        loginInteractor.login(inputData);
+
+        // Assert
+        verify(presenter).presentFailure("Incorrect username or password.");
+    }
+}
